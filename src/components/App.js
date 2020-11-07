e56787f1939c66a460bc3ef17fa547a1a5341abb
Amit <amitkumartiwari527@gmaial.com>
2020-11-07 18:32:37 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App{
-  render()
-  {
-    return (
-      <p>"I am learning React. My life is getting better."<p/>
-    );
-  }
+const App = () => {
+  return (
+    <p>"I am learning React. My life is getting better. "<p/>
+  )
 }
 
 
